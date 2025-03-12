7f302b74715534c58755a225f82e136a9369a86b
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-12 11:01:43 +0100

    created file

---
+++ Worksheet_BONUS/Aufgabe_2.dart
@@ -0,0 +1,20 @@
+// Erstelle zwei Funktionen:
+void printGrades(List<int> grades) {
+  // Gibt alle Noten nacheinander aus
+  // z.B.: Note 1: 2, Note 2: 1, Note 3: 3
+}
+
+void calculateAndPrintAverage(List<int> grades) {
+  // Berechnet den Durchschnitt und gibt aus:
+  // "Der Durchschnitt ist: 2.0"
+}
+
+// Hauptfunktion:
+void analyzeGrades(List<int> grades) {
+  // Ruft beide Funktionen nacheinander auf
+}
+
+void main() {
+  
+
+}
