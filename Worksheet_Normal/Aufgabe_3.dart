54effbe439057edc578d229c39da17548e195868
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-12 10:52:25 +0100

    Aufgabe 3 fertig

---
+++ Worksheet_Normal/Aufgabe_3.dart
@@ -0,0 +1,9 @@
+void reverseInitials(String vorname, String nachname) {
+  print("${vorname[vorname.length - 1]}. ${nachname[nachname.length - 1]}.");
+}
+
+
+void main() {
+  reverseInitials("David", "Bowie");
+
+}
